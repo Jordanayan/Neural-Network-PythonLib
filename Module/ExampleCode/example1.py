309becd2051888cabe67171be290ddd1bf1e2cbe
NerdyYoungMen <fsapcontact@gmail.com>
2017-07-06 22:25:12 -0400

    Adding File for Jordan's example Code

---
+++ Module/ExampleCode/example1.py
@@ -0,0 +1,9 @@
+#######################################
+#Neural Network Module Example 1
+#example1.py
+#
+#(c) 2017 Jordan M. Hofstrad
+#######################################
+
+#Imports NeuralNetwork Library
+import NeuralNetwork
